0d8913c597b167429c7c376e01c9a8e61cb55442
zcm0705 <cuimei@brandeis.edu>
2017-06-09 12:03:28 +0800

    edit function

--- public/javascript/event.js
+++ public/javascript/event.js
@@ -8,29 +8,17 @@ $("#add-btn").on('click', function(ev) {
 	var end = $("#end-time").val();
 	var eName = $("#event-name").val();
 	var eDay = $("#event-day").val();
-	var eInfo = $("textarea").val();
+	var eDesc = $("textarea").val();
+	var event = new Event(start, end, parseInt(eDay), eName, eDesc);
+	placeEvent(event);
+
 	$("textarea").val('');
 	$("#start-time").val('');
 	$("#end-time").val('');
 	$("#event-name").val('');
-	
-	//json: store data
-	var event = {
-		startTime: start,
-		endTime: end,
-		name: eName,
-		day: parseInt(eDay),
-		info: eInfo
-	};
-	placeEvent(event);
 
 	//send to mongoDB
-	$.ajax({
-		url:"/history",
-		method:"PUT",
-		data:{operation:"add event"}
-	});
-	
+	cloud.put("Create " + event.name +" event");
 });
 
 function convertTime(time) {
@@ -41,29 +29,55 @@ function convertTime(time) {
 }
 
 function placeEvent(event) {
-	var top = 65 + 40 * (convertTime(event.startTime) - 9);
-	var height = 40 * (convertTime(event.endTime) - convertTime(event.startTime));
-	var left = 140 + (event.day - 1) * 102;
-
 	//create event element
 	var eventDiv = $("<div>").text(event.name).appendTo($("body"));
-	eventDiv.css({top:top, left: left, height: height});
+	eventDiv.css({top:event.top(), left: event.left(), height: event.height()});
 	eventDiv.addClass("eventDiv");
 	
 	var open = false;
 	eventDiv.on('click', function(ev) {
+		if (ev.target != eventDiv.get(0) && ev.target.className != "info") {
+		   return;
+		}
 		if (open) {
-			closeEvent(ev, top, left, height);
+			closeEvent(eventDiv, event);
 			open = false;
 		} else {
-			openEvent(ev, event);
+			openEvent(eventDiv, event);
 			open = true;
 		}
 	});
 }
 
-function openEvent(ev, event) {
-	var evDiv = $(ev.target);
+function Event(start, end, day, name, description) {
+	this.startTime = start;
+	this.endTime = end;
+	this.day = day;
+	this.name = name;
+	this.description = description;
+
+	this.top = function() {
+		return 65 + 40 * (convertTime(this.startTime) - 9);
+	};
+	this.height = function() {
+		return 40 * (convertTime(this.endTime) - convertTime(this.startTime));
+	};
+	this.left = function() {
+		return 140 + (this.day - 1) * 102;
+	}
+}
+
+var cloud = {
+	put: function(log) {
+		$.ajax({
+			url:"/history",
+			method:"PUT",
+			data:{operation:log}
+		});	
+	}
+};
+
+function openEvent(evDiv, event) {
 	evDiv.css("z-index", 5);
 	//enlarge the individual event
 	evDiv.animate({
@@ -73,76 +87,59 @@ function openEvent(ev, event) {
 		left: "100px",
 	}, 1000, function() {
 		//show detailed info
-		var info = $("<div class='info'>");
-		var $start = $("<div>").text("start time: " + event.startTime).addClass("display-time").appendTo(info);
-		var $end = $("<div>").text("end time: " + event.endTime).addClass("display-time").appendTo(info);
-		var $desc = $("<div>").text(event.info).addClass("display-info").appendTo(info);
+		var info = $("<div>", {class:"info"});
+		var start = $("<input>", {placeholder: "Type the start time", type: "text"}).val(event.startTime).appendTo(info);
+		var end = $("<input>", {placeholder: "Type the end time", type: "text"}).val(event.endTime).appendTo(info);
+		var desc = $("<textarea>", {placeholder: "Description"}).val(event.description).appendTo(info);
 		evDiv.append(info);
-		
-		//create delete button
+		start.on("change", function() {
+			cloud.put("Change start time from " + event.startTime + " to " + this.value);
+			event.startTime = this.value;
+		});
+		end.on("change", function() {
+			cloud.put("Change end time from " + event.endTime + " to " + this.value);
+			event.endTime = this.value;
+		});
+		desc.on("change", function() {
+			cloud.put("Change description from " + event.description + " to " + this.value);
+			event.description = this.value;
+		});
+
 		var del = $("<button class='delBtn'>");
 		del.text("Delete");
 		evDiv.append(del);
 		del.css({left: "700px", top: "10px", position: "absolute", marginBottom: "0px"});
 		del.on('click', function () {
-			$.ajax({
-				url:"/history",
-				method:"PUT",
-				data:{operation:"delete event"}
-			});
+			cloud.put("Delete" + event.name +" event");
 			evDiv.remove();
 		});
-
-		//edit function
 		
 	});
 }
 
-function closeEvent(ev, top, left, height) {
-	var evDiv = $(ev.target);
+function closeEvent(evDiv, event) {
 	evDiv.css("z-index", 1);
 	//change back to the normal status
+
 	evDiv.animate({
 		width: "102px",
-		height: height + "px",
-		top: top + "px",
-		left: left + "px"
+		height: event.height() + "px",
+		top: event.top() + "px",
+		left: event.left() + "px"
 	}, 1000, function() {
 		//after the animation, remove button
-		// $(".edit-Btn").remove();
 		$(".delBtn").remove();
 		$(".info").remove();
-		$(".editBtn").remove();
 	});
 }
 
 //set some default events for testing
-var yoga = {
-	startTime: "10:00",
-	endTime: "11:00",
-	day: 2,
-	name: "Yoga",
-	info: "yoga refreshes your brain"
-};
-
+var yoga = new Event("10:00", "11:00", 2, "yoga", "yoga refreshes your brain");
 placeEvent(yoga);
 
-var GER20B = {
-	startTime: "9:00",
-	endTime: "12:30",
-	day: 3,
-	name: "GER 20B: Continuing German",
-	info: "Provides an overview of the relationship between nature and culture in North America. Covers Native Americans, the European invasion, the development of a market system of resource extraction and etc."
-};
 
+var GER20B = new Event("9:00", "12:30", 3, "GER 20B: Continuing German", "Provides an overview of the relationship between nature and culture in North America. Covers Native Americans, the European invasion, the development of a market system of resource extraction and etc.");
 placeEvent(GER20B);
 
-var SOC147A = {
-	startTime: "10:30",
-	endTime: "13:00",
-	day: 1,
-	name: "SOC 147A: Sustainable and Resilient Cities",
-	info: "Studies innovations in the U.S and around the world that enhance urban sustainability"
-};
-
+var SOC147A = new Event("10:30", "13:00", 1, "SOC 147A: Sustainable and Resilient Cities", "Studies innovations in the U.S and around the world that enhance urban sustainability");
 placeEvent(SOC147A);
